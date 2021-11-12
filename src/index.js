eeed8e591dc8f500e320fc97186532ef2bef13ef
Andrey Kruglik <isekaiguy@protonmail.com>
2021-11-12 19:38:58 +0700

    Layout ready

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app/App';
+import './style/style.scss';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
+
