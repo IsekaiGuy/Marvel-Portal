7844a2dc21f690365611f511ffa913fd14898302
Andrey Kruglik <isekaiguy@protonmail.com>
2021-11-21 23:48:16 +0700

    Added page 404

---
+++ src/pages/Page404/Page404.js
@@ -0,0 +1,18 @@
+import "./Page404.scss";
+
+const Page404 = () => {
+  return (
+    <div className="error">
+      <div className="error__content">
+        <h1 className="error__title">ERROR 404: NOT FOUND</h1>
+        <h2 className="error__subtitle">Mission failed...</h2>
+        <p className="error__text">
+          Make sure you typed in the page address correctly or go back to your
+          previous page.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Page404;
